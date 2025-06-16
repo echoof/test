5cbd49eebf8594410cda58d2358b8a29005346d0
echoof <54647766+echoof@users.noreply.github.com>
2025-06-16 16:53:40 +0800

    Update first.py

--- first.py
+++ first.py
@@ -1,16 +1,1 @@
-def hello():
-	print("hello world")
-	print("ssh commit")
-        print("change on mster")
-	# bug resolved
 
-def windows()
-    print("hello windows")
-
-def linux()
-	print("hello linux ")
-	print("hello windows")
-
-
-def new_function():
-	print("new function")
